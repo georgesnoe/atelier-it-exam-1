f7e3b29136d42c28c7de8b1208114b9d9d6717dd
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-16 14:33:08 +0000

    Implemented afficher resultat code

--- calculatrice/script.js
+++ calculatrice/script.js
@@ -2,9 +2,12 @@ let juger = document.getElementById("juger");
 let nombre_1 = document.getElementById("nombre_1");
 let nombre_2 = document.getElementById("nombre_2");
 let operateur = document.getElementById("operateur");
+let jugement = document.getElementById("jugement");
 
 
 juger.onclick = () => {
+    let resultat = 0;
+    let op = "";
     if (nombre_1.value.trim() == "") {
         afficherErreur_1();
     } else if (nombre_2.value.trim() == "") {
@@ -12,15 +15,23 @@ juger.onclick = () => {
     } else {
         switch (operateur.value) {
             case '+':
+                op = "+";
+                resultat = parseFloat(nombre_1) + parseFloat(nombre_2);
                 break;
             case '-':
+                op = "-";
+                resultat = parseFloat(nombre_1) - parseFloat(nombre_2);
                 break;
             case '*':
+                op = "*";
+                resultat = parseFloat(nombre_1) * parseFloat(nombre_2);
                 break;
             case '/':
+                op = "/";
+                resultat = parseFloat(nombre_1) / parseFloat(nombre_2);
                 break;
         }
-        afficherResultat();
+        afficherResultat(`${nombre_1} ${op} ${nombre_2}`, resultat);
     }
 }
 
@@ -28,4 +39,8 @@ function afficherErreur_1() { }
 
 function afficherErreur_2() { }
 
-function afficherResultat(expression, resultat) { }
+function afficherResultat(expression, resultat) {
+    jugement.innerHTML = `
+        <p>Le résultat de ${expression} = ${resultat}</p>
+    `;
+}
