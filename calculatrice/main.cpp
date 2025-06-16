40a0a8e6fee0b5793271ad082fb3f3b4d4119970
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-16 14:14:52 +0000

    Finished implementing calculator in C++

---
+++ calculatrice/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main()
+{
+    float nombre_1, nombre_2, resultat;
+    char operateur;
+
+    std::cout << "Programme de calculatrice basique" << std::endl;
+    std::cout << std::endl
+              << std::endl;
+    std::cin.clear();
+    std::cout << "Veuiller entrer le premier nombre : ";
+    std::cin >> nombre_1;
+    std::cin.clear();
+    std::cout << "Veuiller entrer le deuxieme nombre : ";
+    std::cin >> nombre_2;
+    std::cin.clear();
+    std::cout << "Veuiller entrer l'opérateur (+, -, *, /) : ";
+    std::cin >> operateur;
+    std::cin.clear();
+
+    switch (operateur)
+    {
+    case '+':
+        resultat = nombre_1 + nombre_2;
+        break;
+    case '-':
+        resultat = nombre_1 - nombre_2;
+        break;
+    case '*':
+        resultat = nombre_1 * nombre_2;
+        break;
+    case '/':
+        resultat = nombre_1 / nombre_2;
+        break;
+    }
+
+    std::cout << std::endl
+              << std::endl;
+    std::cout << "Le résultat de " << nombre_1 << " " << operateur << " " << nombre_2 << " = " << resultat << std::endl;
+
+    return 0;
+}
