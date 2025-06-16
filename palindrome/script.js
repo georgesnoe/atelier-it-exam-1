750856c57e97a49bc287aa0868be645fc51b65bb
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-16 13:47:21 +0000

    Make code logic for palindrome in javascript

--- palindrome/script.js
+++ palindrome/script.js
@@ -1,9 +1,31 @@
 let juger = document.getElementById("juger");
 let valeur = document.getElementById("valeur");
+let erreur = document.getElementById("erreur");
 
 juger.onclick = () => {
     // Si l'utilisateur n'a saisi aucune valeur
     if (valeur.value.trim() == "") {
-
+        afficherErreur();
+    } else {
+        // Enlever les espaces blancs au début et à la fin
+        let saisie = valeur.value.trim();
+        // Valeur inversée de la saisie
+        let reverseSaisie = "";
+        for (let i = saisie.length; i > 0; i++) {
+            // Les indices vont de 0 à (n - 1)
+            reverseSaisie += saisie[i - 1];
+        }
+        // Comparer les valeurs
+        if (saisie == reverseSaisie) {
+            afficherPositif();
+        } else {
+            afficherNegatif();
+        }
     }
 }
+
+function afficherErreur() { }
+
+function afficherPositif() { }
+
+function afficherNegatif() { }
