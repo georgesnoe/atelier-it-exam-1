e46ed53510f34d22c66e837d4895cda00ba659ed
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-16 13:42:35 +0000

    Styled palindrome error field

--- palindrome/script.js
+++ palindrome/script.js
@@ -1,1 +1,9 @@
-// Code javascript
+let juger = document.getElementById("juger");
+let valeur = document.getElementById("valeur");
+
+juger.onclick = () => {
+    // Si l'utilisateur n'a saisi aucune valeur
+    if (valeur.value.trim() == "") {
+
+    }
+}
